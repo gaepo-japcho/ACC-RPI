595a0885f2ab1387fadfceba304b8f3b614358ff
Park JongBeum <dev@parkjb.com>
2026-04-09 01:09:21 +0900

    feat (logger): add logger

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import acc_can as can
 import acc_fusion as fusion
 import acc_hmi as hmi
+from common import get_logger
+
+logger = get_logger(__name__)
 
 def main() -> None:
+    logger.info("ACC 시스템 시작")
     pass
     # TODOs
     # initialize can module
