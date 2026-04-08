af107a0ad5979d1f879e052d131aefb7fc0f294d
Park JongBeum <dev@parkjb.com>
2026-04-08 22:36:24 +0900

    init: initialize rpi python app

---
+++ main.py
@@ -0,0 +1,13 @@
+import acc_can as can
+import acc_fusion as fusion
+import acc_hmi as hmi
+
+def main() -> None:
+    pass
+    # TODOs
+    # initialize can module
+    # initialize fusion module
+    # initialize and launch hmi
+
+if __name__ == "__main__":
+    main()
